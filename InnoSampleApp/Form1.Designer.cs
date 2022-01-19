e80add9ffe8ae480d8c13b1de50edcd3079e34ce
Arindam Sarkar <so.9cool@gmail.com>
2022-01-19 11:47:06 +0530

    Added files

---
+++ InnoSampleApp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace InnoSampleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHello
+            // 
+            this.btnHello.Location = new System.Drawing.Point(35, 80);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(192, 90);
+            this.btnHello.TabIndex = 0;
+            this.btnHello.Text = "Say hello to me";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 272);
+            this.Controls.Add(this.btnHello);
+            this.Name = "Form1";
+            this.Text = "Inno Setup Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHello;
+    }
+}
+
---
+++ InnoSampleApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestComponent;
+
+namespace InnoSampleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            Hello Hello = new Hello();
+            MessageBox.Show( Hello.sayHello());
+        }
+    }
+}
